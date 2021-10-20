3f4d929bb4852b6bf4cef705f5bf67c6fa61a9f6
Ivan <ivan.frontcoder@gmail.com>
2021-10-20 16:42:20 +0500

    .

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -104,7 +104,7 @@ $(document).ready(function () {
 	      speed: 500, // Скорость перехода слайдов
 	      autoplay: false, // Автопрокрутка
 	      autoplaySpeed: 2000, // Скорость автопрокрутки
-	      infinite: true, // Зацикленное пролистывание
+	      infinite: false, // Зацикленное пролистывание
 	      adaptiveHeight: true, // Подгоняет высоту слайдера под элемент слайда
 	      swipe: true, // Перелистывание пальцем
 	      draggable: true, // Перелистывание мышью
@@ -160,10 +160,62 @@ $(document).ready(function () {
 	  }
 	}
 	sliderTeam($('#sliderTeam'))
+	
+	function sliderReview(slider) {
+	  if (slider.length) {
+	    slider.slick({
+	      slidesToShow: 2, // Сколько слайдов показывать на экране
+	      slidesToScroll: 1, // Сколько слайдов пролистывать за раз
+	      dots: false, // Пагинация
+	      arrows: true, // Стрелки
+	      prevArrow: '<div class="slider-arrow slider-arrow--prev"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22"><defs><clipPath><path fill-opacity=".514" d="m-7 1024.36h34v34h-34z"/></clipPath><clipPath><path fill-opacity=".472" d="m-6 1028.36h32v32h-32z"/></clipPath></defs><path d="m345.44 248.29l-194.29 194.28c-12.359 12.365-32.397 12.365-44.75 0-12.354-12.354-12.354-32.391 0-44.744l171.91-171.91-171.91-171.9c-12.354-12.359-12.354-32.394 0-44.748 12.354-12.359 32.391-12.359 44.75 0l194.29 194.28c6.177 6.18 9.262 14.271 9.262 22.366 0 8.099-3.091 16.196-9.267 22.373" transform="matrix(.03541-.00013.00013.03541 2.98 3.02)"/></svg></div>',
+	      nextArrow: '<div class="slider-arrow slider-arrow--next"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22"><defs><clipPath><path fill-opacity=".514" d="m-7 1024.36h34v34h-34z"/></clipPath><clipPath><path fill-opacity=".472" d="m-6 1028.36h32v32h-32z"/></clipPath></defs><path d="m345.44 248.29l-194.29 194.28c-12.359 12.365-32.397 12.365-44.75 0-12.354-12.354-12.354-32.391 0-44.744l171.91-171.91-171.91-171.9c-12.354-12.359-12.354-32.394 0-44.748 12.354-12.359 32.391-12.359 44.75 0l194.29 194.28c6.177 6.18 9.262 14.271 9.262 22.366 0 8.099-3.091 16.196-9.267 22.373" transform="matrix(.03541-.00013.00013.03541 2.98 3.02)"/></svg></div>',
+	      speed: 500, // Скорость перехода слайдов
+	      autoplay: false, // Автопрокрутка
+	      autoplaySpeed: 2000, // Скорость автопрокрутки
+	      infinite: false, // Зацикленное пролистывание
+	      adaptiveHeight: true, // Подгоняет высоту слайдера под элемент слайда
+	      swipe: true, // Перелистывание пальцем
+	      draggable: true, // Перелистывание мышью
+	      swipeToSlide: true, // Свободное перелистывание (на произвольное кол-во слайдов)
+	      responsive: [ // Адаптация
+	        {
+	          breakpoint: breakSm,
+	          settings: {
+	            slidesToShow: 1,
+	          }
+	        },
+	      ]
+	    })
+	    .on('setPosition', function (event, slick) {
+	      slick.$slides.css('height', slick.$slideTrack.height() + 'px');
+	    });
+	  }
+	}
+	sliderReview($('#sliderReview'))
 	// libs-settings/fullpage_settings.js
 	// libs-settings/tinyscrollbar-settings.js
 	// libs-settings/tooltipster-settings.js
-	// libs-settings/yandex-map-settings.js
+	// YandexMap
+	ymaps.ready(function () {
+	  initYandexMap();
+	});
+	function initYandexMap() {
+	  var myMap;
+	  myMap = new ymaps.Map("yandexMap", {
+	    center: [55.76, 37.64], // Центр карты
+	    zoom: 7, // Коэффициент масштаба карты
+	    controls: [ // Элементы управления
+	      'zoomControl',
+	      // 'searchControl',
+	      // 'typeSelector',
+	      // 'fullscreenControl',
+	      // 'routeButtonControl'
+	    ]
+	  });
+	  myMap.behaviors.disable('scrollZoom'); // Отключить изменение масштаба скроллом мыши
+	  
+	}
 	// libs-settings/google-map-settings.js
 	// mailto-ajax.js
 
--- js-app/libs-settings/slick_settings.js
+++ js-app/libs-settings/slick_settings.js
@@ -88,7 +88,7 @@ function sliderPortfolio(slider) {
       speed: 500, // Скорость перехода слайдов
       autoplay: false, // Автопрокрутка
       autoplaySpeed: 2000, // Скорость автопрокрутки
-      infinite: true, // Зацикленное пролистывание
+      infinite: false, // Зацикленное пролистывание
       adaptiveHeight: true, // Подгоняет высоту слайдера под элемент слайда
       swipe: true, // Перелистывание пальцем
       draggable: true, // Перелистывание мышью
@@ -144,3 +144,36 @@ function sliderTeam(slider) {
   }
 }
 sliderTeam($('#sliderTeam'))
+
+function sliderReview(slider) {
+  if (slider.length) {
+    slider.slick({
+      slidesToShow: 2, // Сколько слайдов показывать на экране
+      slidesToScroll: 1, // Сколько слайдов пролистывать за раз
+      dots: false, // Пагинация
+      arrows: true, // Стрелки
+      prevArrow: '<div class="slider-arrow slider-arrow--prev"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22"><defs><clipPath><path fill-opacity=".514" d="m-7 1024.36h34v34h-34z"/></clipPath><clipPath><path fill-opacity=".472" d="m-6 1028.36h32v32h-32z"/></clipPath></defs><path d="m345.44 248.29l-194.29 194.28c-12.359 12.365-32.397 12.365-44.75 0-12.354-12.354-12.354-32.391 0-44.744l171.91-171.91-171.91-171.9c-12.354-12.359-12.354-32.394 0-44.748 12.354-12.359 32.391-12.359 44.75 0l194.29 194.28c6.177 6.18 9.262 14.271 9.262 22.366 0 8.099-3.091 16.196-9.267 22.373" transform="matrix(.03541-.00013.00013.03541 2.98 3.02)"/></svg></div>',
+      nextArrow: '<div class="slider-arrow slider-arrow--next"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22"><defs><clipPath><path fill-opacity=".514" d="m-7 1024.36h34v34h-34z"/></clipPath><clipPath><path fill-opacity=".472" d="m-6 1028.36h32v32h-32z"/></clipPath></defs><path d="m345.44 248.29l-194.29 194.28c-12.359 12.365-32.397 12.365-44.75 0-12.354-12.354-12.354-32.391 0-44.744l171.91-171.91-171.91-171.9c-12.354-12.359-12.354-32.394 0-44.748 12.354-12.359 32.391-12.359 44.75 0l194.29 194.28c6.177 6.18 9.262 14.271 9.262 22.366 0 8.099-3.091 16.196-9.267 22.373" transform="matrix(.03541-.00013.00013.03541 2.98 3.02)"/></svg></div>',
+      speed: 500, // Скорость перехода слайдов
+      autoplay: false, // Автопрокрутка
+      autoplaySpeed: 2000, // Скорость автопрокрутки
+      infinite: false, // Зацикленное пролистывание
+      adaptiveHeight: true, // Подгоняет высоту слайдера под элемент слайда
+      swipe: true, // Перелистывание пальцем
+      draggable: true, // Перелистывание мышью
+      swipeToSlide: true, // Свободное перелистывание (на произвольное кол-во слайдов)
+      responsive: [ // Адаптация
+        {
+          breakpoint: breakSm,
+          settings: {
+            slidesToShow: 1,
+          }
+        },
+      ]
+    })
+    .on('setPosition', function (event, slick) {
+      slick.$slides.css('height', slick.$slideTrack.height() + 'px');
+    });
+  }
+}
+sliderReview($('#sliderReview'))
